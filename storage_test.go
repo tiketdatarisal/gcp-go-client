eaa539480b6deb19a6996a4d9d9a9209f1adf6ea
Risal <risal.risal@tiket.com>
2021-05-23 20:36:22 +0700

    Update storage_test.go

--- storage_test.go
+++ storage_test.go
@@ -39,3 +39,22 @@ func TestStorage_AllObjects(t *testing.T) {
 		fmt.Println(strings.Join(objects, "\n"))
 	}
 }
+
+func TestStorage_CreateAndGetObject(t *testing.T) {
+	ctx := NewContext()
+	data := `1,2,3,4,5,6,7,8,9,0`
+
+	err := ctx.Storage.CreateObject("dev_dms_bucket", "20210522/sample.csv", []byte(data))
+	if err != nil {
+		t.Fatalf("failed to create object data with default credentials, details: %v", err)
+	}
+
+	result, err := ctx.Storage.GetObject("dev_dms_bucket", "20210522/sample.csv")
+	if err != nil {
+		t.Fatalf("failed to get object data with default credentials, details: %v", err)
+	}
+
+	if data != string(result) {
+		t.Error("data do not match!")
+	}
+}
